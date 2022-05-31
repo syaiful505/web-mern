2bcbe888c062c5b56179439f08ed7936f39cd312
syaiful505 <syaifulstyle505@gmail.com>
2022-06-01 01:45:54 +0700

    Configure React Router

--- src/App.js
+++ src/App.js
@@ -6,15 +6,19 @@ import About from './components/About';
 import Services from './components/Services';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 
 function App() {
   return (
     <>
       <Navbar />
-      <Home />
-      <About />
-      <Services />
-      <Contact />
+      <Routes>
+        <Route path="/" caseSensitive={false} element={<Home />} />
+        <Route path="/about" caseSensitive={false} element={<About />} />
+        <Route path="/services" caseSensitive={false} element={<Services />} />
+        <Route path="/contact" caseSensitive={false} element={<Contact />} />
+      </Routes>
       <Footer />
     </>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
           <a class="nav-link" href="#">Contact</a>
         </li>
       </ul>
-      <a class="navbar-brand fw-bolder fs-4 mx-auto" href="#">Navbar</a>
+      <a class="navbar-brand fw-bolder fs-4 mx-auto" href="#">SYAIFUL_STYLE</a>
       <button className="btn btn-outline-primary ms-auto px-4 rounded-pill"><i className="fa fa-sign-in me-2"></i>Login</button>
       <button className="btn btn-outline-primary ms-2 px-4 rounded-pill"><i className="fa fa-user me-2"></i>Register</button>
     </div>
