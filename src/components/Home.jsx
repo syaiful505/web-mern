b63a5575e681b3d67e5d9566b184828d2957971d
syaiful505 <syaifulstyle505@gmail.com>
2022-06-01 02:07:09 +0700

    Update Configure Router

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import About from './About';
+import Services from './Services';
+import Contact from './Contact';
+import { NavLink } from 'react-router-dom';
 
 const Home = () => {
     return (
@@ -12,13 +16,16 @@ const Home = () => {
                         </h1>
                         <p className="lead text-center fs-4 mb-5 text-white"> Lorem ipsum </p>
                         <div className="buttons d-flex justify-content-center">
-                            <button className="btn btn-light me-4 rounded-pill px-4 py-2">Get Quote</button>
-                            <button className="btn btn-outline-light rounded-pill px-4 py-2">Our Services</button>
+                            <NavLink to="/contact" className="btn btn-light me-4 rounded-pill px-4 py-2">Get Quote</NavLink>
+                            <NavLink to="/services" className="btn btn-outline-light rounded-pill px-4 py-2">Our Services</NavLink>
                         </div>
                     </div>
                 </div>
             </div>
         </section>
+        <About />
+        <Services />
+        <Contact />
       </div>
     );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,30 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return (
       <div>
-        <nav class="navbar navbar-expand-lg navbar-light shadow">
-  <div class="container">
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg navbar-light shadow">
+  <div className="container">
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">About</a>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/about">About</NavLink>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Services</a>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/services">Services</NavLink>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Contact</a>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/contact">Contact</NavLink>
         </li>
       </ul>
-      <a class="navbar-brand fw-bolder fs-4 mx-auto" href="#">SYAIFUL_STYLE</a>
+      <NavLink className="navbar-brand fw-bolder fs-4 mx-auto" to="/">SYAIFUL_STYLE</NavLink>
       <button className="btn btn-outline-primary ms-auto px-4 rounded-pill"><i className="fa fa-sign-in me-2"></i>Login</button>
       <button className="btn btn-outline-primary ms-2 px-4 rounded-pill"><i className="fa fa-user me-2"></i>Register</button>
     </div>
